a8e1deb5f0011ea824faf240771c9f71e5fbe464
David Acosta <david.acosta.ing@gmail.com>
2025-07-22 16:34:05 -0500

    Centering board in screen

--- src/main.c
+++ src/main.c
@@ -11,26 +11,38 @@
 const char tile_chars[TILE_TYPES] = {'#', '@', '$', '%', '&'};
 const int font_size = 20;
 const int font_spacing = 1;
+const int screen_width = 800;
+const int screen_height = 800;
 
 char board[BOARD_SIZE][BOARD_SIZE];
+Vector2 grid_origin;
 
 char RandomTile(){
     return tile_chars[rand() % TILE_TYPES];
 }
 
 void InitBoard(){
-    for(int x = 0; x < BOARD_SIZE; x++){
-        for(int y = 0; y < BOARD_SIZE; y++){
+    for(int y = 0; y < BOARD_SIZE; y++){
+        for(int x = 0; x < BOARD_SIZE; x++){
             board[x][y] = RandomTile();
         }
     }
+    int grid_width = BOARD_SIZE * TILE_SIZE;
+    int grid_height = BOARD_SIZE * TILE_SIZE;
+
+    grid_origin = (Vector2){
+        ((screen_width / 2) - grid_width) / 2, //GetScreenWidth()
+        screen_height - (grid_height + TILE_SIZE * 0.9)   // GetScreenHeight() (screen_height - grid_height) / 2
+    };
+
 }
 
 void DrawTiles(){
     for(int y = 0; y < BOARD_SIZE ; y++){
         for(int x = 0; x < BOARD_SIZE; x++){
             Rectangle tileCell = {
-                x * TILE_SIZE, y * TILE_SIZE, // Tile position in x and y
+                grid_origin.x + (x * TILE_SIZE),
+                grid_origin.y + (y * TILE_SIZE), // Tile position in x and y
                 TILE_SIZE, TILE_SIZE  // Tile size in x and y
             };
             DrawRectangleLinesEx(tileCell, 1, DARKGRAY);
@@ -39,7 +51,7 @@ void DrawTiles(){
                 GetFontDefault(),
                 TextFormat("%c", board[y][x]), // Converts from Char to String the characters from board[][]
                 (Vector2) { // position of the ASCII character
-                    tileCell.x + 12, tileCell.y + 12
+                    tileCell.x + 12, tileCell.y + 8
                 },
                 font_size,
                 font_spacing,
@@ -49,8 +61,7 @@ void DrawTiles(){
     }
 }
 int main(void){
-    const int screen_width = 800;
-    const int screen_height = 450;
+
     InitWindow(screen_width, screen_height, "MATCH3 ASCII");
     SetTargetFPS(60);
     srand(time(NULL));
